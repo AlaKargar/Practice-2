b418bd71a5ffe8a72d3d08b9c677fa27cc0ba9e8
Ala Kargar <135884426+AlaKargar@users.noreply.github.com>
2023-12-21 08:33:13 +0330

    practice4.py

--- practice4.py
+++ practice4.py
@@ -1,7 +1,11 @@
 import random
-x = (random.randint(1, 6))
-print(x)
-
-if x == 6:
-    print("win! again!")
-    y = print(random.randint(1, 6))
+z=True
+while z!=False:
+  x = (random.randint(1,6))
+  print(x)
+  if x == 6:
+    print("again!")
+    y= (random.randint(1,6))
+    print(y)
+    z=False
+    break
